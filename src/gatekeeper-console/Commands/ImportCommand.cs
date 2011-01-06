f1d00b90d2e79486088b14baccac0b8608c27f49
Chamith Siriwardena <chamith@gmail.com>
2011-01-06 03:32:51 +0000

    * src/gatekeeper-console/Commands/ImportCommand.cs:

--- src/gatekeeper-console/Commands/ImportCommand.cs
+++ src/gatekeeper-console/Commands/ImportCommand.cs
@@ -10,8 +10,21 @@ namespace Gatekeeper.ConsoleApp.Commands
 		
 		public override void Execute (string[] args)
 		{
+			if(args.Length == 0)
+				throw new ArgumentException("file name not provided");
+			else if(args.Length > 1)
+				throw new ArgumentException("too many arguments");
+			
 			XMLImporter importer = new XMLImporter();
-			importer.Import(args[0]);
+			
+			try
+			{
+				importer.Import(args[0]);
+			}
+			catch(Exception ex)
+			{
+				Console.WriteLine("something went wrong.. \n{0}", ex.ToString());
+			}
 		}
 	}
 }
