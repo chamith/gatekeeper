5163e23e53c7c6e4aa72b2a34106b479fb7291c8
Chamith Siriwardena <chamith@gmail.com>
2011-08-16 20:59:47 +0530

    adding comments

--- src/gatekeeper/ApplicationUser.cs
+++ src/gatekeeper/ApplicationUser.cs
@@ -5,15 +5,25 @@ using Gatekeeper.Core;
 
 namespace Gatekeeper
 {
-    public class ApplicationUser:BaseEntity
-    {
-        public Application Application { get; set; }
 
-        public User User { get; set; }
-		
-		public Role Role {
-			get;
-			set;
-		}
-    }
+	/// <summary>
+	/// Represents a user of an Application. 
+	/// </summary>
+	public class ApplicationUser : BaseEntity
+	{
+		/// <summary>
+		/// Gets or sets the Application. 
+		/// </summary>
+		public Application Application { get; set; }
+
+		/// <summary>
+		/// Gets or sets the User. 
+		/// </summary>
+		public User User { get; set; }
+
+		/// <summary>
+		/// Gets or sets the Role. 
+		/// </summary>
+		public Role Role { get; set; }
+	}
 }
